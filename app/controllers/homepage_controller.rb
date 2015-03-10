11dde507d0c887c28d6ace83d4001f8ff9ff24c8
PRATAP <pratap224@gmail.com>
2015-03-10 20:42:43 +0530

    homepage

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,10 +1,10 @@
 class HomepageController < ApplicationController
   
   def index
-  	@anouncements = Refannouncement.all
+  	@anouncements = Refannouncement.last(3)
     @blogpost =  Refblog.last(2)
     @testimonials = Reftestimonial.all
-    @posts = Refforum.last(7)
+    @posts = Refforum.last(6)
     @social = Refinery::Page.where(:slug => 'home_social_groups').first
     
   end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150309140804) do
+ActiveRecord::Schema.define(version: 20150310121014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(version: 20150309140804) do
     t.integer  "position"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "description"
   end
 
   create_table "refinery_blogposts", force: true do |t|
